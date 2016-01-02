c7fe09cfb94a13f7fa0f0656745a76bde74467ed
Jose Ignacio Hita Barraza <ignaciohita@gmail.com>
2016-01-02 15:37:29 +0100

    Initial commit

--- www/js/main.js
+++ www/js/main.js
@@ -21,26 +21,37 @@ function registrarIntento() {
     if (pluginListo) {
         document.getElementById("botonJugar").style.display = "none";
 
-        navigator.compass.getCurrentHeading(function (posicion) {
-            if (Math.abs(posicion.magneticHeading) < 1) {
-                navigator.notification.alert("¡Enhorabuena! ¡Has encontrado la posición norte con una precisión del " + (100 - Math.abs(posicion.magneticHeading)).toString() + "%!");
-            } else {
-                navigator.notification.alert("¡Fallaste! ¡Estás a " + Math.abs(posicion.magneticHeading).toString() + " grados del norte!");
-            }
+        navigator.compass.getCurrentHeading(
+            function (posicion) {
+                var gradosAbsolutos;
 
-            document.getElementById("botonJugar").style.display = "block";
-        }, function (posicionError) {
-            switch (posicionError.code) {
-            case CompassError.COMPASS_INTERNAL_ERR:
-                navigator.notification.alert("Error en la brújula");
-                break;
-            case CompassError.COMPASS_NOT_SUPPORTED:
-                navigator.notification.alert("No hay brújula");
-                break;
-            default:
-                navigator.notification.alert("Error desconocido");
-                break;
+                if (posicion.magneticHeading > 180) {
+                    gradosAbsolutos = 360 - posicion.magneticHeading;
+                } else {
+                    gradosAbsolutos = posicion.magneticHeading;
+                }
+
+                if ((gradosAbsolutos) < 1) {
+                    navigator.notification.alert("¡Enhorabuena! ¡Has encontrado la posición norte con una precisión del " + (100 - gradosAbsolutos).toString() + "%!");
+                } else {
+                    navigator.notification.alert("¡Fallaste! ¡Estás a " + Math.round(gradosAbsolutos).toString() + " grados del norte!");
+                }
+
+                document.getElementById("botonJugar").style.display = "block";
+            },
+            function (posicionError) {
+                switch (posicionError.code) {
+                case CompassError.COMPASS_INTERNAL_ERR:
+                    navigator.notification.alert("Error en la brújula");
+                    break;
+                case CompassError.COMPASS_NOT_SUPPORTED:
+                    navigator.notification.alert("No hay brújula");
+                    break;
+                default:
+                    navigator.notification.alert("Error desconocido");
+                    break;
+                }
             }
-        });
+        );
     }
 }
